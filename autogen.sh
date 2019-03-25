9a3e0dbf9613a0a9253daf57f079274dad8d003c
Jerome Soumagne <jsoumagne@hdfgroup.org>
2019-03-25 12:24:17 -0500

    Fix copyright

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
+#      
 #
-# Copyright by The HDF Group.                                              
-# All rights reserved.                                                     
-#                                                                          
-# This file is part of HDF5. The full HDF5 copyright notice, including     
-# terms governing use, modification, and redistribution, is contained in   
-# the files COPYING and Copyright.html.  COPYING can be found at the root  
-# of the source code distribution tree; Copyright.html can be found at the 
-# root level of an installed copy of the electronic document set and is    
-# linked from the top-level documents page.  It can also be found at       
-# http://hdfgroup.org/HDF5/doc/Copyright.html.  If you do not have access  
-# to either file, you may request a copy from help@hdfgroup.org.           
-#
-
 # A script to reconfigure autotools for the DAOS VOL connector.
 #
 # IMPORTANT OS X NOTE

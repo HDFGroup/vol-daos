2388e9da2b3f400a1702e63d3aeea517a185488c
Jerome Soumagne <jsoumagne@hdfgroup.org>
2019-04-09 18:22:32 -0500

    Disable address sanitizer in build script and use bash

--- test/scripts/jelly_build.sh
+++ test/scripts/jelly_build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Running build script from repository"
 echo "(current dir is: $PWD)"
@@ -15,13 +15,13 @@ export HDF5_VOL_DAOS_BUILD_CONFIGURATION="Debug"
 export HDF5_VOL_DAOS_DASHBOARD_MODEL="Nightly"
 export HDF5_VOL_DAOS_DO_COVERAGE="true"
 export HDF5_VOL_DAOS_DO_MEMCHECK="false"
-export HDF5_VOL_DAOS_MEMORYCHECK_TYPE="AddressSanitizer"
+#export HDF5_VOL_DAOS_MEMORYCHECK_TYPE="AddressSanitizer"
 
 export CC=`which gcc`
 export GCOV=`which gcov`
 
 # get back to the testing script location
 pushd $HDF5_VOL_DAOS_ROOT
-ctest -S $HDF5_VOL_DAOS_ROOT/source/test/scripts/jenkins_script.cmake -VV --output-on-failure 2>&1 > $HDF5_VOL_DAOS_ROOT/last_build.log
+ctest -S $HDF5_VOL_DAOS_ROOT/source/test/scripts/jelly_script.cmake -VV --output-on-failure 2>&1 > $HDF5_VOL_DAOS_ROOT/last_build.log
 popd
 

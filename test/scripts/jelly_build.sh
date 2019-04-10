7495a70267c698131dee9861eba28b26fcbefb1d
Jerome Soumagne <jsoumagne@hdfgroup.org>
2019-04-10 10:24:03 -0500

    Update test script on jelly for sanitizer and release builds

--- test/scripts/jelly_build.sh
+++ test/scripts/jelly_build.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/bin/bash -l
 
 echo "Running build script from repository"
 echo "(current dir is: $PWD)"
 
 module load GCC/7.2.0-2.29
+
+# Spack
+export SPACK_ROOT=/mnt/hdf/jsoumagne/spack
+source $SPACK_ROOT/share/spack/setup-env.sh
 source <(spack module tcl loads --dependencies daos)
 source <(spack module tcl loads --dependencies hdf5@daos-develop)
 
@@ -13,15 +17,27 @@ export HDF5_VOL_DAOS_ROOT=/mnt/hdf/jsoumagne/daos_vol_test
 # set up testing configuration
 export HDF5_VOL_DAOS_BUILD_CONFIGURATION="Debug"
 export HDF5_VOL_DAOS_DASHBOARD_MODEL="Nightly"
-export HDF5_VOL_DAOS_DO_COVERAGE="true"
-export HDF5_VOL_DAOS_DO_MEMCHECK="false"
-#export HDF5_VOL_DAOS_MEMORYCHECK_TYPE="AddressSanitizer"
 
 export CC=`which gcc`
 export GCOV=`which gcov`
 
 # get back to the testing script location
 pushd $HDF5_VOL_DAOS_ROOT
-ctest -S $HDF5_VOL_DAOS_ROOT/source/test/scripts/jelly_script.cmake -VV --output-on-failure 2>&1 > $HDF5_VOL_DAOS_ROOT/last_build.log
+
+export HDF5_VOL_DAOS_DO_COVERAGE="true"
+export HDF5_VOL_DAOS_DO_MEMCHECK="false"
+ctest -S $HDF5_VOL_DAOS_ROOT/source/test/scripts/jelly_script.cmake -VV --output-on-failure 2>&1 > $HDF5_VOL_DAOS_ROOT/last_build_coverage.log
+
+export HDF5_VOL_DAOS_DO_COVERAGE="false"
+export HDF5_VOL_DAOS_DO_MEMCHECK="true"
+export HDF5_VOL_DAOS_MEMORYCHECK_TYPE="AddressSanitizer"
+ctest -S $HDF5_VOL_DAOS_ROOT/source/test/scripts/jelly_script.cmake -VV --output-on-failure 2>&1 > $HDF5_VOL_DAOS_ROOT/last_build_memcheck.log
+
+export HDF5_VOL_DAOS_BUILD_CONFIGURATION="RelWithDebInfo"
+export HDF5_VOL_DAOS_DO_COVERAGE="false"
+export HDF5_VOL_DAOS_DO_MEMCHECK="false"
+unset  HDF5_VOL_DAOS_MEMORYCHECK_TYPE
+ctest -S $HDF5_VOL_DAOS_ROOT/source/test/scripts/jelly_script.cmake -VV --output-on-failure 2>&1 > $HDF5_VOL_DAOS_ROOT/last_build_release.log
+
 popd
 

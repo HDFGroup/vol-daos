f373bb2a4c8523727197497b8a4bb58a876bdd3f
Jerome Soumagne <jsoumagne@hdfgroup.org>
2020-03-19 12:19:59 -0500

    Fix version number in daily test script

--- test/scripts/jelly_script.cmake
+++ test/scripts/jelly_script.cmake
@@ -105,7 +105,7 @@ if(HDF5_VOL_DAOS_DO_MEMCHECK OR HDF5_VOL_DAOS_MEMORYCHECK_TYPE)
 endif()
 
 # Build name referenced in cdash
-set(CTEST_BUILD_NAME "daos-0.6-gcc-${lower_hdf5_vol_daos_build_configuration}${lower_hdf5_vol_daos_memorycheck_type}${coverage_suffix}")
+set(CTEST_BUILD_NAME "daos-0.9-gcc-${lower_hdf5_vol_daos_build_configuration}${lower_hdf5_vol_daos_memorycheck_type}${coverage_suffix}")
 
 set(dashboard_binary_name hdf5_vol_daos-${lower_hdf5_vol_daos_build_configuration})
 if(NOT hdf5_vol_daos_build_shared)

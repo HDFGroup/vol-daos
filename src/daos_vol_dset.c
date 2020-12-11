f6abb8b5c9367fffdf4568819da6169c1e4a3717
Neil Fortner <nfortne2@hdfgroup.org>
2020-12-11 15:36:55 -0600

    Add missing H5_daos_task_wait conversion to dataset write.  Update vol test submodule.

--- src/daos_vol_dset.c
+++ src/daos_vol_dset.c
@@ -3947,8 +3947,8 @@ done:
         } /* end if */
         else {
             /* Block until operation completes */
-            if(H5_daos_progress(int_req, H5_DAOS_PROGRESS_WAIT) < 0)
-                D_DONE_ERROR(H5E_DATASET, H5E_CANTINIT, FAIL, "can't progress scheduler");
+            if(H5_daos_task_wait(int_req->finalize_task, NULL) < 0)
+                D_DONE_ERROR(H5E_DATASET, H5E_CANTINIT, FAIL, "can't wait for request to finish");
 
             /* Check for failure */
             if(int_req->status < 0)

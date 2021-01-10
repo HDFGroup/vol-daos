fadab3b6de958ce2cfed70c283621c96ab0e019b
Neil Fortner <nfortne2@hdfgroup.org>
2021-01-09 18:58:44 -0600

    Fix inappropriate usage of H5_DAOS_PREP_REQ_DONE.  Fix uninitialized use warning.

--- src/daos_vol_req.c
+++ src/daos_vol_req.c
@@ -444,7 +444,7 @@ H5_daos_req_enqueue(H5_daos_req_t *req, tse_task_t *first_task,
     H5_daos_op_pool_scope_t scope, hbool_t collective, hbool_t sync)
 {
     H5_daos_op_pool_t **parent_cur_op_pool[4];
-    H5_daos_op_pool_t *tmp_pool;
+    H5_daos_op_pool_t *tmp_pool = NULL;
     H5_daos_op_pool_t *tmp_new_pool_alloc = NULL;
     H5_daos_op_pool_t *tmp_new_pool_alloc_2 = NULL;
     hbool_t create_new_pool;

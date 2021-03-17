37236d9fd61960e4849b6acc57474d05a5c59853
Jordan Henderson <jhenderson@hdfgroup.org>
2021-03-17 13:31:58 -0500

    Update documentation for routines in public header

--- src/daos_vol_public.h
+++ src/daos_vol_public.h
@@ -56,15 +56,138 @@ typedef uint64_t H5_daos_snap_id_t;
 extern "C" {
 #endif
 
-H5VL_DAOS_PUBLIC herr_t H5daos_init(uuid_t pool_uuid, const char *pool_grp, const char *pool_svcl);
-H5VL_DAOS_PUBLIC herr_t H5daos_term(void);
-H5VL_DAOS_PUBLIC herr_t H5Pset_fapl_daos(hid_t fapl_id, MPI_Comm comm, MPI_Info info);
-H5VL_DAOS_PUBLIC herr_t H5daos_set_object_class(hid_t plist_id, char *object_class);
-H5VL_DAOS_PUBLIC ssize_t H5daos_get_object_class(hid_t plist_id, char *object_class, size_t size);
-H5VL_DAOS_PUBLIC herr_t H5daos_set_root_open_object_class(hid_t fapl_id, char *object_class);
-H5VL_DAOS_PUBLIC ssize_t H5daos_get_root_open_object_class(hid_t fapl_id, char *object_class, size_t size);
-H5VL_DAOS_PUBLIC herr_t H5daos_set_all_ind_metadata_ops(hid_t accpl_id, hbool_t is_independent);
-H5VL_DAOS_PUBLIC herr_t H5daos_get_all_ind_metadata_ops(hid_t accpl_id, hbool_t *is_independent);
+/**
+ * Initialize this VOL connector by connecting to the pool and registering
+ * the connector with the library. \pool_uuid identifies the UUID of the
+ * pool to connect to. \pool_grp and \pool_svcl respectively identify the
+ * server group name and pool service replica rank list to use when connecting
+ * to DAOS. These may be NULL, in which case a default group name and service
+ * replica rank list are used.
+ *
+ * \param pool_uuid [IN]    DAOS pool UUID
+ * \param pool_grp  [IN]    Process set name of the DAOS servers managing the pool
+ * \param pool_svcl [IN]    Comma-separated list of pool service replica ranks
+ *
+ * \return Non-negative on success/Negative on failure
+ */
+H5VL_DAOS_PUBLIC herr_t
+H5daos_init(uuid_t pool_uuid, const char *pool_grp, const char *pool_svcl);
+
+/**
+ * Shut down the DAOS VOL.
+ *
+ * \return Non-negative on success/Negative on failure
+ */
+H5VL_DAOS_PUBLIC herr_t
+H5daos_term(void);
+
+/**
+ * Modify the file access property list to use the DAOS VOL connector.
+ * \comm and \info identify the communicator and info object used to coordinate
+ * actions on file create, open, flush, and close.
+ *
+ * \param fapl_id [IN]      File access property list
+ * \param comm    [IN]      MPI communicator
+ * \param info    [IN]      MPI info
+ *
+ * \return Non-negative on success/Negative on failure
+ */
+H5VL_DAOS_PUBLIC herr_t
+H5Pset_fapl_daos(hid_t fapl_id, MPI_Comm comm, MPI_Info info);
+
+/**
+ * Sets the provided DAOS object class on the given property list.
+ * Refer to the DAOS documentation for a list of object classes and
+ * descriptions of them.
+ *
+ * If \plist_id is a File, Group, Datatype, Dataset or Map creation
+ * property list, this setting will affect objects created using
+ * that property list (a File creation property list affects only
+ * the root group and global metadata objects).
+ *
+ * If \plist_id is a File access property list, this setting will
+ * affect all objects created in files opened with the property list,
+ * except for those with their object class specified via an object
+ * creation property list, as above.
+ *
+ * If the root group of a file is created with a non-default object
+ * class, later opens of the file must use a File access property
+ * list with the same object class set on it by using
+ * H5daos_set_root_open_object_class().
+ *
+ * \param plist_id     [IN]     Object creation or File access property list
+ * \param object_class [IN]     DAOS object class string
+ *
+ * \return Non-negative on success/Negative on failure
+ */
+H5VL_DAOS_PUBLIC herr_t
+H5daos_set_object_class(hid_t plist_id, char *object_class);
+
+/**
+ * Retrieves the DAOS object class set on the given property list.
+ *
+ * \param plist_id     [IN]     Object creation or File access property list
+ * \param object_class [OUT]    Object class string output buffer
+ * \param size         [IN]     Size of object class string output buffer
+ *
+ * \return Length of object class string (excluding null terminator) on success/Negative on failure
+ */
+H5VL_DAOS_PUBLIC ssize_t
+H5daos_get_object_class(hid_t plist_id, char *object_class, size_t size);
+
+/**
+ * Sets the DAOS object class to use for opening the root group
+ * of a file on the given File access property list. The specified
+ * object class should match the object class used to create the
+ * root group.
+ *
+ * \param fapl_id      [IN]     File access property list
+ * \param object_class [IN]     DAOS object class string
+ *
+ * \return Non-negative on success/Negative on failure
+ */
+H5VL_DAOS_PUBLIC herr_t
+H5daos_set_root_open_object_class(hid_t fapl_id, char *object_class);
+
+/**
+ * Retrieves the DAOS object class for opening the root group of a file
+ * from the given File access property list.
+ *
+ * \param fapl_id      [IN]     File access property list
+ * \param object_class [OUT]    Object class string output buffer
+ * \param size         [IN]     Size of object class string output buffer
+ *
+ * \return Length of object class string (excluding null terminator) on success/Negative on failure
+ */
+H5VL_DAOS_PUBLIC ssize_t
+H5daos_get_root_open_object_class(hid_t fapl_id, char *object_class, size_t size);
+
+/**
+ * Modifies the given access property list to indicate that all
+ * metadata I/O operations should be performed independently. By
+ * default, metadata reads are independent and metadata writes
+ * are collective.
+ *
+ * \param accpl_id       [IN]   File, Link or Reference access property list
+ * \param is_independent [IN]   Boolean flag indicating whether all metadata I/O should be independent
+ *
+ * \return Non-negative on success/Negative on failure
+ */
+H5VL_DAOS_PUBLIC herr_t
+H5daos_set_all_ind_metadata_ops(hid_t accpl_id, hbool_t is_independent);
+
+/**
+ * Retrieves the independent metadata I/O setting from the given
+ * access property list.
+ *
+ * \param accpl_id       [IN]   File, Link or Reference access property list
+ * \param is_independent [OUT]  Boolean flag indicating whether all metadata I/O is independent
+ *
+ * \return Non-negative on success/Negative on failure
+ */
+H5VL_DAOS_PUBLIC herr_t
+H5daos_get_all_ind_metadata_ops(hid_t accpl_id, hbool_t *is_independent);
+
 #ifdef DSINC
 H5VL_DAOS_PUBLIC herr_t H5daos_snap_create(hid_t loc_id,
     H5_daos_snap_id_t *snap_id);
@@ -74,6 +197,9 @@ H5VL_DAOS_PUBLIC herr_t H5Pset_daos_snap_open(hid_t fapl_id,
     H5_daos_snap_id_t snap_id);
 #endif
 
+/**
+ * API routines for external testing
+ */
 H5VL_DAOS_PUBLIC herr_t H5daos_get_poh(hid_t file_id, daos_handle_t *poh);
 H5VL_DAOS_PUBLIC herr_t H5daos_get_pool_uuid(hid_t file_id, uuid_t *pool_uuid);
 H5VL_DAOS_PUBLIC herr_t H5daos_get_global_svcl(d_rank_list_t *svcl);

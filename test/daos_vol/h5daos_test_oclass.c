d74c33b0d50fe72b0caf2f1596f6a51ab58c7a7f
Jerome Soumagne <jsoumagne@hdfgroup.org>
2022-05-09 12:40:28 -0500

    test: fix format and test macros

--- test/daos_vol/h5daos_test_oclass.c
+++ test/daos_vol/h5daos_test_oclass.c
@@ -45,14 +45,17 @@ check_plist_oclass(hid_t plist_id, const char *exp_oclass)
     char    oclass_buf[10];
 
     if ((oclass_size = H5daos_get_object_class(plist_id, oclass_buf, sizeof(oclass_buf))) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     if (oclass_size != (ssize_t)strlen(oclass_buf)) {
-        H5_FAILED() AT() printf("incorrect object class size\n");
+        H5_FAILED();
+        AT();
+        printf("incorrect object class size\n");
         goto error;
     } /* end if */
     if (strcmp(oclass_buf, exp_oclass)) {
-        H5_FAILED()
-        AT() printf("returned object class \"%s\" does not match expected: \"%s\"\n", oclass_buf, exp_oclass);
+        H5_FAILED();
+        AT();
+        printf("returned object class \"%s\" does not match expected: \"%s\"\n", oclass_buf, exp_oclass);
         goto error;
     } /* end if */
 
@@ -78,109 +81,114 @@ test_oclass(hid_t fcpl_id, hid_t fapl_id, const char *exp_root_oclass, const cha
 
     /* Create dataspace */
     if ((space_id = H5Screate_simple(1, dims, NULL)) < 0)
-        TEST_ERROR
+        TEST_ERROR;
 
     /* Create file */
     if ((file_id = H5Fcreate(FILENAME, H5F_ACC_TRUNC, fcpl_id, fapl_id)) < 0)
-        TEST_ERROR
+        TEST_ERROR;
 
     /* Create default group */
     if ((obj_id = H5Gcreate2(file_id, DEF_GROUP_NAME, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT)) < 0)
-        TEST_ERROR
+        TEST_ERROR;
 
     /* Check group's object class */
     if ((plist_id = H5Gget_create_plist(obj_id)) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     if (0 != check_plist_oclass(plist_id, exp_group_oclass)) {
-        H5_FAILED() AT() printf("object class check failed for default group\n");
+        H5_FAILED();
+        AT();
+        printf("object class check failed for default group\n");
         goto error;
     } /* end if */
     if (H5Pclose(plist_id) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     plist_id = -1;
 
     if (H5Gclose(obj_id) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     obj_id = -1;
 
     /* Create custom group */
     if ((plist_id = H5Pcreate(H5P_GROUP_CREATE)) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     if (H5daos_set_object_class(plist_id, "S2") < 0)
-        TEST_ERROR
+        TEST_ERROR;
     if ((obj_id = H5Gcreate2(file_id, MOD_GROUP_NAME, H5P_DEFAULT, plist_id, H5P_DEFAULT)) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     if (H5Pclose(plist_id) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     plist_id = -1;
 
     /* Check group's object class */
     if ((plist_id = H5Gget_create_plist(obj_id)) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     if (0 != check_plist_oclass(plist_id, "S2")) {
-        H5_FAILED() AT() printf("object class check failed for custom group\n");
+        H5_FAILED();
+        AT();
+        printf("object class check failed for custom group\n");
         goto error;
     } /* end if */
     if (H5Pclose(plist_id) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     plist_id = -1;
 
     if (H5Gclose(obj_id) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     obj_id = -1;
 
     /* Create default dataset */
     if ((obj_id = H5Dcreate2(file_id, DEF_DSET_NAME, H5T_NATIVE_INT, space_id, H5P_DEFAULT, H5P_DEFAULT,
                              H5P_DEFAULT)) < 0)
-        TEST_ERROR
+        TEST_ERROR;
 
     /* Check dataset's object class */
     if ((plist_id = H5Dget_create_plist(obj_id)) < 0)
-        TEST_ERROR
+        TEST_ERROR;
         /** SX is translated to the largest number of targets available on a system. skip this for now */
 #if 0
     if(0 != check_plist_oclass(plist_id, exp_dset_oclass)) {
-        H5_FAILED() AT()
+        H5_FAILED(); AT();
         printf("object class check failed for default dataset\n");
         goto error;
     } /* end if */
 #else
-            (void)
-            exp_dset_oclass; /* silence compiler */
+    (void)exp_dset_oclass; /* silence compiler */
 #endif
     if (H5Pclose(plist_id) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     plist_id = -1;
 
     if (H5Dclose(obj_id) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     obj_id = -1;
 
     /* Create custom dataset */
     if ((plist_id = H5Pcreate(H5P_DATASET_CREATE)) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     if (H5daos_set_object_class(plist_id, "S1") < 0)
-        TEST_ERROR
+        TEST_ERROR;
     if ((obj_id = H5Dcreate2(file_id, MOD_DSET_NAME, H5T_NATIVE_INT, space_id, H5P_DEFAULT, plist_id,
                              H5P_DEFAULT)) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     if (H5Pclose(plist_id) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     plist_id = -1;
 
     /* Check dataset's object class */
     if ((plist_id = H5Dget_create_plist(obj_id)) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     if (0 != check_plist_oclass(plist_id, "S1")) {
-        H5_FAILED() AT() printf("object class check failed for custom dataset\n");
+        H5_FAILED();
+        AT();
+        printf("object class check failed for custom dataset\n");
         goto error;
     } /* end if */
     if (H5Pclose(plist_id) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     plist_id = -1;
 
     if (H5Dclose(obj_id) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     obj_id = -1;
 
     /* Check all object classes after reopening objects, both before and after
@@ -188,102 +196,107 @@ test_oclass(hid_t fcpl_id, hid_t fapl_id, const char *exp_root_oclass, const cha
     for (i = 0; i < 2; i++) {
         /* Check file's object class */
         if ((plist_id = H5Fget_create_plist(file_id)) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         if (0 != check_plist_oclass(plist_id, exp_root_oclass)) {
-            H5_FAILED() AT() printf("object class check failed for file%s\n", i ? " after file reopen" : "");
+            H5_FAILED();
+            AT();
+            printf("object class check failed for file%s\n", i ? " after file reopen" : "");
             goto error;
         } /* end if */
         if (H5Pclose(plist_id) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         plist_id = -1;
 
         /* Open root group and check its object class */
         if ((obj_id = H5Gopen2(file_id, "/", H5P_DEFAULT)) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         if ((plist_id = H5Gget_create_plist(obj_id)) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         if (0 != check_plist_oclass(plist_id, exp_root_oclass)) {
-            H5_FAILED()
-            AT() printf("object class check failed for root group%s\n", i ? " after file reopen" : "");
+            H5_FAILED();
+            AT();
+            printf("object class check failed for root group%s\n", i ? " after file reopen" : "");
             goto error;
         } /* end if */
         if (H5Pclose(plist_id) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         plist_id = -1;
         if (H5Gclose(obj_id) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         obj_id = -1;
 
         /* Open default group and check its object class */
         if ((obj_id = H5Gopen2(file_id, DEF_GROUP_NAME, H5P_DEFAULT)) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         if ((plist_id = H5Gget_create_plist(obj_id)) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         if (0 != check_plist_oclass(plist_id, exp_group_oclass)) {
-            H5_FAILED()
-            AT() printf("object class check failed for default group after %s reopen", i ? "file" : "object");
+            H5_FAILED();
+            AT();
+            printf("object class check failed for default group after %s reopen", i ? "file" : "object");
             goto error;
         } /* end if */
         if (H5Pclose(plist_id) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         plist_id = -1;
         if (H5Gclose(obj_id) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         obj_id = -1;
 
         /* Open custom group and check its object class */
         if ((obj_id = H5Gopen2(file_id, MOD_GROUP_NAME, H5P_DEFAULT)) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         if ((plist_id = H5Gget_create_plist(obj_id)) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         if (0 != check_plist_oclass(plist_id, "S2")) {
-            H5_FAILED()
-            AT() printf("object class check failed for custom group after %s reopen", i ? "file" : "object");
+            H5_FAILED();
+            AT();
+            printf("object class check failed for custom group after %s reopen", i ? "file" : "object");
             goto error;
         } /* end if */
         if (H5Pclose(plist_id) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         plist_id = -1;
         if (H5Gclose(obj_id) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         obj_id = -1;
 
         /* Open default dataset and check its object class */
         if ((obj_id = H5Dopen2(file_id, DEF_DSET_NAME, H5P_DEFAULT)) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         if ((plist_id = H5Dget_create_plist(obj_id)) < 0)
-            TEST_ERROR
+            TEST_ERROR;
             /** SX is translated to the largest number of targets available on a system. skip this for now */
 #if 0
         if(0 != check_plist_oclass(plist_id, exp_dset_oclass)) {
-            H5_FAILED() AT()
+            H5_FAILED(); AT();
             printf("object class check failed for default dataset after %s reopen", i ? "file" : "object");
             goto error;
         } /* end if */
 #endif
         if (H5Pclose(plist_id) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         plist_id = -1;
         if (H5Dclose(obj_id) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         obj_id = -1;
 
         /* Open custom dataset and check its object class */
         if ((obj_id = H5Dopen2(file_id, MOD_DSET_NAME, H5P_DEFAULT)) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         if ((plist_id = H5Dget_create_plist(obj_id)) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         if (0 != check_plist_oclass(plist_id, "S1")) {
-            H5_FAILED()
-            AT()
-                printf("object class check failed for custom dataset after %s reopen", i ? "file" : "object");
+            H5_FAILED();
+            AT();
+            printf("object class check failed for custom dataset after %s reopen", i ? "file" : "object");
             goto error;
         } /* end if */
         if (H5Pclose(plist_id) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         plist_id = -1;
         if (H5Dclose(obj_id) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         obj_id = -1;
 
         /* No need to reopen the file on the second iteration */
@@ -292,16 +305,16 @@ test_oclass(hid_t fcpl_id, hid_t fapl_id, const char *exp_root_oclass, const cha
 
         /* Reopen file */
         if (H5Fclose(file_id) < 0)
-            TEST_ERROR
+            TEST_ERROR;
         if ((file_id = H5Fopen(FILENAME, H5F_ACC_RDONLY, fapl_id)) < 0)
-            TEST_ERROR
+            TEST_ERROR;
     } /* end for */
 
     /* Close */
     if (H5Fclose(file_id) < 0)
-        TEST_ERROR
+        TEST_ERROR;
     if (H5Sclose(space_id) < 0)
-        TEST_ERROR
+        TEST_ERROR;
 
     PASSED();
 
